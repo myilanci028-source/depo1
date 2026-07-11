72138729f67e517c98482336f3535102fa8dbd20
myilanci028-source <myilanci028@gmail.com>
2026-07-11 03:03:50 +0300

    Add iOS application entry point

---
+++ ios-barcode-keyboard/App/YilanciogluBarcodeKeyboardApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct YilanciogluBarcodeKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
